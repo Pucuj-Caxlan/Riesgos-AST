f2d96604e3c92712f21cd8f1c9e1aedf7122f041
Pucuj-Caxlan <mfcoflores@icloud.com>
2025-06-22 02:02:34 -0600

    main.py

--- main.py
+++ main.py
@@ -30,3 +30,8 @@ def llenar_riesgo():
 
     except Exception as e:
         return jsonify({"mensaje": f"Error: {str(e)}"}), 500
+        if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto por variable de entorno
+    app.run(host="0.0.0.0", port=port)
+
