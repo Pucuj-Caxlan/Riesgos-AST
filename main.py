1c405eaa5616908648922b8c271527cc759bd9d6
Pucuj-Caxlan <mfcoflores@icloud.com>
2025-06-22 00:56:56 -0600

    main.py

--- main.py
+++ main.py
@@ -43,4 +43,6 @@ def llenar_riesgo():
     return jsonify({"mensaje": "Riesgo registrado correctamente", "fila_insertada": fila})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
